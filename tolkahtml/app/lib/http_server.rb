ef8a3f15373496c2ab385bd3ce046fcad6803e59
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2024-04-22 10:07:43 +0200

    lektionsjobb 22 april

--- tolkahtml/app/lib/http_server.rb
+++ tolkahtml/app/lib/http_server.rb
@@ -1,7 +1,7 @@
 require 'socket'
 require_relative 'router'
 require_relative 'request'
-
+require 'debug'
 
 class HTTPServer
 
@@ -19,7 +19,12 @@ class HTTPServer
         router.add_route("GET", "/user/:id") do
             File.read("./views/index.html")
         end
-        router.add_route("GET", "/banan/:id/paj")
+        router.add_route("GET", "/banan/:id/paj") do |id|
+            "<h1>Weet #{id}</h1>"
+        end
+        router.add_route("GET", "/senap/:id/paj/:hej") do |id, hej|
+            "<h1>Weet #{id.to_i * hej.to_i}</h1>"
+        end
         # router.add_route()
 
         router.add_route("GET", "/") do
@@ -41,7 +46,10 @@ class HTTPServer
             match = router.match_route(request)
             if match
                 status = 200
-                html = match[:block].call
+               
+                html = match[:block].call(match[:params])
+            elsif File.file?("./public#{request.resource}")
+                p "found file"
             else
                 #finns filen i public?
 
--- tolkahtml/app/lib/router.rb
+++ tolkahtml/app/lib/router.rb
@@ -5,59 +5,43 @@ class Router
         @routes = []
     end
 
-
     def add_route(method, route, &blk)
-        # @routes.store(route,method)
 
-        # if route.match(/:/)
         if route.include?(":")
             regex_route = route.gsub(/(:\w+)/, '(\w+)')
-
-#            puts regex_route
             regex_route = regex_route.gsub((/\//), '\/')
-#            puts regex_route
+            regex_route = Regexp.new(regex_route)
 
-            # @routes[regex_route] = method
-            # @routes[/^#{regex_route}$/] = method
-            @routes << {route: route, method: method, block: blk}
+            @routes << {route: regex_route, method: method, block: blk}
         else
             @routes << {route: route, method: method, block: blk}
-
-            # @routes[route] = method
-
         end
 
     end
 
-    
-    #             regex_route = route.gsub(/(:\w+)/, '(\w+)')
-    #             regex_route = regex_route.gsub(/\//, '\/')
-    #             @routes[/^#{regex_route}$/] = method
-
-
     def match_route(request)
-        # binding.break
         p @routes
         @method = request.instance_variable_get(:@method)
         @route = request.instance_variable_get(:@resource)
 
         @routes.each do |route|
-            # route2 = route[:route]
             
             if route[:method] == @method
-                
-                if @route == route[:route]
-                    p "route match"
+               
+                if params = @route.match(route[:route])
+                    if params[2]
+                        route[:params] = params[1..]
+                    else
+                        route[:params] = params[1]
+                    end
                     return route
                 end
-            
+
             end
-            
         end
     
         puts "No matching route found for #{request.resource}"
         nil
     end
-
 end
     
